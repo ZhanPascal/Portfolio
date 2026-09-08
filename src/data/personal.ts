2a4be1f16dd5b6b678fc8cf7effd09e73856b131
ZhanPascal <102191853+ZhanPascal@users.noreply.github.com>
2026-09-08 23:50:09 +0200

    Update linkedIn link

--- src/data/personal.ts
+++ src/data/personal.ts
@@ -2,7 +2,7 @@ export const personalInfo = {
   name: 'Pascal ZHAN',
   email: 'pascal.zhan.pro@gmail.com',
   github: 'https://github.com/ZhanPascal',
-  linkedin: 'https://www.linkedin.com/in/pascal-zhan/',
+  linkedin: 'https://www.linkedin.com/in/pascal-zhan-9741b9226/',
   availability: {
     internshipPeriod: 'April - September 2026',
     alternanceStart: 'September 2026',
